8e2e0d6bd3455d546f8b835fbbaa9c9b02a3a09f
Jang4360 <jooyoon4360@gmail.com>
2025-05-05 00:54:06 +0900

    update

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -17,7 +17,8 @@ export default function SignupPage() {
 
   const router = useRouter();
 
-  const handleSignup = async () => {
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
     setEmailError(false);
     setPasswordMismatch(false);
 
