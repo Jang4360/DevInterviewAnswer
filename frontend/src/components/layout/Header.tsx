56e8b9f4227d22bc3b5e221e7803af82d6a58953
Jang4360 <jooyoon4360@gmail.com>
2025-05-06 23:19:12 +0900

    update frontend

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,38 +1,29 @@
 "use client";
-
 import { FC } from "react";
 import Link from "next/link";
 import { FiUser } from "react-icons/fi";
-
 interface HeaderProps {
   toggleSidebar: () => void;
 }
 
 const Header: FC<HeaderProps> = ({ toggleSidebar }) => {
   return (
-    <header className="flex items-center justify-between bg-surface text-white px-4 sm:px-6 py-4 border-b border-gray-700 w-full">
-      {/* 왼쪽 여백 */}
-      <div className="w-16 sm:w-24">
-        <button onClick={toggleSidebar} className="text-lg sm:text-xl">
-          ☰
-        </button>
-      </div>
-
-      {/* 중앙 Dev Interview */}
+    <header className="flex items-center justify-between bg-surface text-white px-6 py-4 border-b border-gray-700 w-full">
+      {/* 중앙 DevInterview */}
       <div className="flex-1 flex justify-center">
         <Link href="/review">
-          <h1 className="text-lg sm:text-xl font-bold cursor-pointer hover:text-[#B3B3B3]">
+          <h1 className="text-xl font-bold cursor-pointer ml-8 sm:ml-32 hover:text-[#B3B3B3]">
             Dev Interview
           </h1>
         </Link>
       </div>
 
-      {/* 오른쪽 버튼 */}
-      <div className="flex items-center gap-3 sm:gap-6">
+      {/* 오른쪽 - Create 버튼 + User 아이콘 */}
+      <div className="flex items-center ">
         <Link href="/create">
           <button
             id="create-btn"
-            className="border px-2 sm:px-3 py-1 rounded text-sm sm:text-lg hover:bg-[#303030] transition"
+            className="cursor-pointer border px-3 py-1 mr-10 rounded text-lg hover:bg-[#303030] transition"
           >
             Create
           </button>
--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -7,18 +7,18 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const pathname = usePathname();
   const isAuthPage = pathname === "/login" || pathname === "/signup";
 
   if (isAuthPage) return <>{children}</>;
 
   return (
-    <div className="flex h-screen bg-[#212121] overflow-hidden relative">
+    <div className="flex h-screen bg-[#212121] overflow-hidden relative ">
       {/* 사이드바 토글 버튼 */}
       <button
         onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        className="cursor-pointer fixed top-4 left-4 z-50 text-white sm:hidden"
+        className="cursor-pointer fixed top-4 left-4 z-50 text-white hover:text-[#B3B3B3]"
       >
         <FiMenu size={24} />
       </button>
@@ -29,11 +29,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {/* 오른쪽 컨텐츠 영역 */}
       <div
         className={`flex flex-col transition-all duration-500 ease-in-out ${
-          isSidebarOpen ? "sm:ml-60" : "ml-0"
+          isSidebarOpen ? "ml-60" : "ml-0"
         } w-full`}
       >
         <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
-        <main className="p-4 sm:p-6">{children}</main>
+        <main className="p-6">{children}</main>
       </div>
     </div>
   );
