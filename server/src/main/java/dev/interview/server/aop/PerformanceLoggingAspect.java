314ab20b7904b109fff61846d9427b81916878c8
Jang4360 <jooyoon4360@gmail.com>
2025-05-12 17:07:17 +0900

    update aop code

---
+++ server/src/main/java/dev/interview/server/aop/PerformanceLoggingAspect.java
@@ -0,0 +1,22 @@
+package dev.interview.server.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class PerformanceLoggingAspect {
+    @Around("execution(* dev.interview.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long duration = System.currentTimeMillis() - start;
+
+        log.info("[PERFORMANCE] {} executed in {} ms", joinPoint.getSignature(), duration);
+        return result;
+    }
+}
