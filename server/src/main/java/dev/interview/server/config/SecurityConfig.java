1fa0004eb6e87b5399afe0346fb828f5fc13c776
Jang4360 <jooyoon4360@gmail.com>
2025-05-13 22:53:08 +0900

    update jmx file

--- server/src/main/java/dev/interview/server/config/SecurityConfig.java
+++ server/src/main/java/dev/interview/server/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import dev.interview.server.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.web.ProjectedPayload;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
